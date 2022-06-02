f533e2a2420864d6b49487e298d5d6d4046a9e47
optovolokn0 <dimathesokolov@gmail.com>
2022-06-02 21:48:10 +0500

    123

--- app/src/main/java/com/school/module5work/API/APIConfig.java
+++ app/src/main/java/com/school/module5work/API/APIConfig.java
@@ -5,5 +5,5 @@ public class APIConfig {
     public static final String LANG = "ru_RU";
     public static final double CITY_LAT = 56.8519000;
     public static final double CITY_LON = 60.6122000;
-    public static final String API_YANDEX_KEY = "ad462c49-4314-4dd3-a5a4-bc358cd61bdb";
+    public static final String API_YANDEX_KEY = "bbb7d75d-f4f2-4567-b2d6-1a6f24aca486";
 }
